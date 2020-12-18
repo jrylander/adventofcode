e0acc0736abe0f256d967bdd30f09898e966d85f
Johan Rylander <johan@rylander.cc>
2020-12-18 13:57:20 +0100

    Dec 04 first

--- dec04-a.py
+++ dec04-a.py
@@ -1,7 +1,14 @@
 from itertools import islice
 
+REQUIRED_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+OPTIONAL_FIELDS = ['cid']
+
 with open("dec04-indata.txt") as indata:
     rows = [line.strip() for line in list(indata)]
+
+    valid = 0
+    invalid = 0
+
     createNew = True
     passports = []
     for row in rows:
@@ -13,7 +20,11 @@ with open("dec04-indata.txt") as indata:
                 createNew = False
             fields = {field.split(':')[0]: field.split(':')[1] for field in row.split(' ')}
             passports[-1] = passports[-1] | fields
+    
+    for passport in passports:
+        if all(passport.get(key) for key in REQUIRED_FIELDS):
+            valid += 1
+        else:
+            invalid += 1
 
-    valid = 0
-    invalid = 0
     print(valid, invalid)
