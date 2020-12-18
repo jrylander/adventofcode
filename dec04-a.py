3ffafd6abe40910dd5bcf55aec2920a9aea508fb
Johan Rylander <johan@rylander.cc>
2020-12-18 13:40:39 +0100

    Use py 3.9 syntax fo take union of dicts

--- dec04-a.py
+++ dec04-a.py
@@ -12,7 +12,7 @@ with open("dec04-indata.txt") as indata:
                 passports.append({})
                 createNew = False
             fields = {field.split(':')[0]: field.split(':')[1] for field in row.split(' ')}
-            passports[-1] = {**passports[-1], **fields}
+            passports[-1] = passports[-1] | fields
 
     valid = 0
     invalid = 0
